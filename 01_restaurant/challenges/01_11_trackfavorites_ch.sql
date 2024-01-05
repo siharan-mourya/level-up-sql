df41217090835f9e1dc2edf4f6eddeafad2b1311
siharan-mourya <153832272+siharan-mourya@users.noreply.github.com>
2024-01-05 16:13:55 +0000

    05.01.2024 SQL practice set 2

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,17 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+SELECT * FROM Customers
+WHERE FirstName = 'Cleo' AND LastName = 'Goldwater';
+
+UPDATE Customers
+SET FavoriteDish = (
+  SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad')
+WHERE CustomerID = 42;
+
+SELECT C.FirstName, C.LastName, C.FavoriteDish, D.Name
+FROM Customers C
+JOIN Dishes D
+ON C.FavoriteDish = D.DishID
+ORDER BY C.FirstName;
--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,18 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT COUNT(O.OrderID) OrdersCount, C.FirstName, C.LastName, C.Email
+FROM Customers C
+JOIN Orders O
+ON C.CustomerID = O.CustomerID
+GROUP BY C.CustomerID
+ORDER BY OrdersCount DESC;
+
+-- to verify the query
+SELECT COUNT(*),CustomerID FROM Orders
+WHERE CustomerID = (SELECT MAX(CustomerID) FROM Orders);
+
+SELECT CustomerID, FirstName, LastName
+FROM Customers
+WHERE CustomerID = (SELECT MAX(CustomerID) FROM Orders);
